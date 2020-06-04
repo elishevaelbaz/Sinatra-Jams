deaf7a8e1e344cc614eed9764b656f14dba01330
github username <github email address>
2020-06-04 15:55:54 -0400

    finished CR of crud

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 require './config/environment'
+require 'pry'
 
 class ApplicationController < Sinatra::Base
 
@@ -9,6 +10,30 @@ class ApplicationController < Sinatra::Base
 
   get "/" do
     erb :welcome
+  end
+
+  get "/artists" do
+    @artists = Artist.all
+    # binding.pry
+    erb :"artists/index"
+  end
+
+  get '/artists/new' do
+    erb :"artists/new"
+  end
+
+  get "/artists/:id" do
+    @artist = Artist.find(params[:id])
+    erb :"artists/show"
+  end
+
+
+  post '/artists' do
+    
+    artist = Artist.create(params[:artist])
+
+    redirect "/artists/#{artist.id}"
+
   end
 
 end
