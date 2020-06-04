9491c74d1cdd2e4195350336f82243f8a441fdde
github username <github email address>
2020-06-04 16:23:26 -0400

    creating separate artist controller

---
+++ app/controllers/artists_controller.rb
@@ -0,0 +1,43 @@
+class ArtistsController < ApplicationController
+
+    get "/artists" do
+        @artists = Artist.all
+        # binding.pry
+        erb :"artists/index"
+    end
+
+    get '/artists/new' do
+        erb :"artists/new"
+    end
+
+    get "/artists/:id" do
+        @artist = Artist.find(params[:id])
+        erb :"artists/show"
+    end
+
+
+    post '/artists' do
+        artist = Artist.create(params[:artist])
+        redirect "/artists/#{artist.id}"
+    end
+
+    get "/artists/:id/edit" do
+        @artist = Artist.find(params[:id])
+
+        erb :"artists/edit"
+    end
+
+    put "/artists/:id" do
+        artist = Artist.find(params[:id])
+        artist.update(params[:artist])
+
+        redirect "/artists/#{artist.id}"
+    end
+
+    delete "/artists/:id" do
+        artist = Artist.find(params[:id])
+        artist.destroy
+
+        redirect "/artists"
+    end
+end
